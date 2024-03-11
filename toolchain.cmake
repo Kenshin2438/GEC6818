3d77d4d65e6a3e01ca18b0f7dfcc077ce54f0088
Kenshin2438 <2953957899@qq.com>
2024-03-11 09:20:37 +0800

    Add flags for C compiler.

--- toolchain.cmake
+++ toolchain.cmake
@@ -1,5 +1,5 @@
 set(CMAKE_SYSTEM_NAME Linux)
-set(CMAKE_SYSTEM_PROCESSOR arm)
+set(CMAKE_SYSTEM_PROCESSOR armv7l)
 
 set(CMAKE_C_COMPILER   "arm-linux-gnueabi-gcc")
 set(CMAKE_CXX_COMPILER "arm-linux-gnueabi-g++")
@@ -23,5 +23,3 @@ set(CMAKE_CXX_FLAGS "-mfloat-abi=softfp -mfpu=neon ${CMAKE_CXX_FLAGS}")
 # cache flags
 set(CMAKE_C_FLAGS "${CMAKE_C_FLAGS}" CACHE STRING "c flags")
 set(CMAKE_CXX_FLAGS "${CMAKE_CXX_FLAGS}" CACHE STRING "c++ flags")
-
-set(CMAKE_EXPORT_COMPILE_COMMANDS ON)
