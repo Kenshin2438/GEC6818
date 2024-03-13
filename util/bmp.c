bfa1702a159bf6f63f1b58b9f14e670c0209daf0
Kenshin2438 <2953957899@qq.com>
2024-03-13 10:56:00 +0800

    Feat(bmp): add flag for centering images

--- util/bmp.c
+++ util/bmp.c
@@ -8,7 +8,7 @@
 
 #include "lcdctl.h"
 
-void bmp_display(struct LCD *lcd, const char *file, size_t r, size_t c) {
+void bmp_display(struct LCD *lcd, const char *file, size_t r_offset, size_t c_offset, int flag) {
   if (lcd == NULL || lcd->addr == NULL) {
     fprintf(stderr, "LCD is uninitialized\n");
     exit(EXIT_FAILURE);
@@ -40,21 +40,30 @@ void bmp_display(struct LCD *lcd, const char *file, size_t r, size_t c) {
   lseek(bmp_fd, offset, SEEK_SET); read(bmp_fd, color, bmp_length);
   close(bmp_fd);
 
-  printf("DEBUG: offset = %zu\n", offset);
-  printf("DEBUG: bmp_H = %d, bmp_W = %d, depth = %u\n", bmp_H, bmp_W, depth);
-  printf("DEBUG: row_size = %zu, bmp_length = %zu\n", row_size, bmp_length);
+  // printf("DEBUG: offset = %zu\n", offset);
+  // printf("DEBUG: bmp_H = %d, bmp_W = %d, depth = %u\n", bmp_H, bmp_W, depth);
+  // printf("DEBUG: row_size = %zu, bmp_length = %zu\n", row_size, bmp_length);
   // clang-format on
+
+  if (flag && r_offset == 0 && c_offset == 0) {
+    r_offset = (SCREEN_H > abs(bmp_H) ? SCREEN_H - abs(bmp_H) : 0) / 2;
+    c_offset = (SCREEN_W > abs(bmp_W) ? SCREEN_W - abs(bmp_W) : 0) / 2;
+  }  // flag for centering images
+
   lcd->clear(lcd);
   size_t idx = 0;
   for (int i = 0; i < abs(bmp_H); i++) {
+    if (i + r_offset >= SCREEN_H) continue;
     for (int j = 0; j < abs(bmp_W); j++) {
+      if (j + c_offset >= SCREEN_W) continue;
       uint32_t A, R, G, B;
       B = color[idx++];
       G = color[idx++];
       R = color[idx++];
       A = (depth == 32 ? color[idx++] : 0);
-      lcd->draw(lcd, (bmp_H < 0 ? i : SCREEN_H - i - 1) + r, (bmp_W > 0 ? j : SCREEN_W - j - 1) + c,
-                (A << 24) | (R << 16) | (G << 8) | B);
+      const size_t r = (bmp_H < 0 ? i + r_offset : SCREEN_H - i - 1 - r_offset);
+      const size_t c = (bmp_W > 0 ? j + c_offset : SCREEN_W - j - 1 - c_offset);
+      lcd->draw(lcd, r, c, (A << 24) | (R << 16) | (G << 8) | B);
     }
     while (idx % row_size != 0) idx++;
   }
--- util/bmp.h
+++ util/bmp.h
@@ -3,6 +3,6 @@
 
 #include "lcdctl.h"
 
-void bmp_display(struct LCD* lcd, const char* file, size_t r, size_t c);
+void bmp_display(struct LCD* lcd, const char* file, size_t r_offset, size_t c_offset, int flag);
 
 #endif  // !BMP_H_
